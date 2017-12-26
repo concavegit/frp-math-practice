c4d289570de54e9d947a01d23c48bda659105667
Kawin Nikomborirak <concavemail@gmail.com>
2017-12-26 04:41:00 +0000

    add trig

--- src/Core.hs
+++ src/Core.hs
@@ -1,8 +1,11 @@
 module Core
   ( randMult
+  , randTrig
   , Mult (..)
+  , Trig (..)
   ) where
 
+import           Control.Arrow
 import           Control.Monad.State
 import           System.Random
 
@@ -11,8 +14,24 @@ data Mult = Mult { multA   :: Int
                  , multAns :: Int
                  }
 
+data Trig = Trig { trigAngle :: Int
+                 , trigOp    :: TrigOp
+                 , trigAns   :: Int
+                 }
+
+data TrigOp = Cos | Sin | Tan deriving (Enum, Show)
+
+instance Random TrigOp where
+  randomR = (. randomR (0, 5)) . first . (!!) . cycle . uncurry enumFromTo
+  random = randomR (Cos, Tan)
+
 rand :: (RandomGen g, Random a) => (a, a) -> State g a
 rand = state . randomR
 
-randMult :: (RandomGen g) => (Int, Int) -> State g Mult
+randMult :: RandomGen g => (Int, Int) -> State g Mult
 randMult ns = (\a b -> Mult a b (a * b)) <$> rand ns <*> rand ns
+
+randTrig :: RandomGen g => State g Trig
+randTrig = (\a -> Trig a <$> toEnum <*> round . (* 1000) . ($ fromIntegral a)
+             . ([cos, sin, tan] !!))
+  <$> rand (0, 90) <*> rand (0, 2)
