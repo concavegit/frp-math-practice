c01413178dfbc2bbe972ad25f7ad944664bc42ea
Kawin Nikomborirak <concavemail@gmail.com>
2017-12-28 22:44:51 +0000

    fix markup and add docs

--- src/Core.hs
+++ src/Core.hs
@@ -9,11 +9,15 @@ import           Control.Arrow
 import           Control.Monad.State
 import           System.Random
 
+-- |Data type representing a multiplication problem.
+-- It consists of the two numbers to be multiplied and the answer.
 data Mult = Mult { multA   :: Int
                  , multB   :: Int
                  , multAns :: Int
                  }
 
+-- |Data type representing a trig problem.
+-- It consists of the angle, operation, and answer two three decimals as an int.
 data Trig = Trig { trigAngle :: Int
                  , trigOp    :: TrigOp
                  , trigAns   :: Int
@@ -28,9 +32,11 @@ instance Random TrigOp where
 rand :: (RandomGen g, Random a) => (a, a) -> State g a
 rand = state . randomR
 
+-- |Random multiplication problem generator.
 randMult :: RandomGen g => (Int, Int) -> State g Mult
 randMult ns = (\a b -> Mult a b (a * b)) <$> rand ns <*> rand ns
 
+-- |Random trig problem generator.
 randTrig :: RandomGen g => State g Trig
 randTrig = (\a -> Trig a <$> toEnum <*> (round :: Integral a => Double -> a)
              . (* 1000) . ($ fromIntegral a) . ([cos, sin, tan] !!))
